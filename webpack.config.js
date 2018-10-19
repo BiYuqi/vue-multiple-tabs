42ecb32d1f4a101ca6212db92b2dac5a12d08ece
loadingmore <biyuqiwan@163.com>
2018-10-19 17:17:38 +0800

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production' ? 'vue-multiple-tabs-use-one-component' : '/',
     filename: 'js/[name].[hash].js'
   },
   module: {
